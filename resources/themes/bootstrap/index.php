bd063ffe7fcca1413953d2e698e53fbd3543bcee
Yangwei@home.pc <916692273@qq.com>
2017-07-25 21:57:36 +0800

    将googleapi的链接更改至本地。

--- resources/themes/bootstrap/index.php
+++ resources/themes/bootstrap/index.php
@@ -13,12 +13,14 @@
         <link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/css/style.css">
 
         <!-- SCRIPTS -->
-        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <!-- <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> -->
+		<script type="text/javascript" src="<?php echo THEMEPATH; ?>/js/jquery.min.js"></script>
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="<?php echo THEMEPATH; ?>/js/directorylister.js"></script>
 
         <!-- FONTS -->
-        <link rel="stylesheet" type="text/css"  href="//fonts.googleapis.com/css?family=Cutive+Mono">
+        <!-- <link rel="stylesheet" type="text/css"  href="//fonts.googleapis.com/css?family=Cutive+Mono"> -->
+		<link rel="stylesheet" type="text/css"  href="<?php echo THEMEPATH; ?>/css/font.css">
 
         <!-- META -->
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
